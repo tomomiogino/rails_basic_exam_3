77c4e7a44ccfc526355303375c11cbb2d2d678e2
tomomi ogino <s4tm83@gmail.com>
2020-12-08 11:53:36 +0900

    add: stationモデルにバリデーション設定

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,3 +1,8 @@
 class Station < ApplicationRecord
+  with_options presence: true do
+    validates :route, length: { maximum: 100 }
+    validates :name, length: { maximum: 80 }
+    validates :minutes_foot, length: { maximum: 4 }, numericality: { greater_than_or_equal_to: 0 }
+  end
   belongs_to :property
 end
