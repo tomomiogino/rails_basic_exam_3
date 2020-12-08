31e1b19afb35096fb2ac9bfb10bd0c5a508bf354
tomomi ogino <s4tm83@gmail.com>
2020-12-08 11:01:31 +0900

    add: propertyモデルにバリデーション設定

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,8 @@
 class Property < ApplicationRecord
+  with_options presence: true do
+    validates :name, length: { maximum: 255 }
+    validates :rent, numericality: { greater_than_or_equal_to: 0 }
+    validates :address, length: { maximum: 255 }
+    validates :age, length: { maximum: 4 }, numericality: { greater_than_or_equal_to: 0 }
+  end
 end
