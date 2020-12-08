70dca2cdbb271cfd841bf583d1d49e28de512fdc
tomomi ogino <s4tm83@gmail.com>
2020-12-08 10:41:15 +0900

    change: rails gでhelperとassetsファイルが生成されないように設定

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,12 @@ Bundler.require(*Rails.groups)
 
 module DevExam
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
