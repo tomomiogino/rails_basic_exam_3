df9b8cc33fe45b33260abf7f35a3ca00e29b378a
tomomi ogino <s4tm83@gmail.com>
2020-12-08 11:21:47 +0900

    add: 物件のCRUD機能を実装

---
+++ app/controllers/properties_controller.rb
@@ -0,0 +1,47 @@
+class PropertiesController < ApplicationController
+  before_action :set_property, only: [:show, :edit, :update, :destroy]
+  def index
+    @properties = Property.all
+  end
+
+  def new
+    @property = Property.new
+  end
+
+  def create
+    @property = Property.new(property_params)
+    if @property.save
+        redirect_to properties_path, notice: %(物件情報を登録しました！)
+      else
+        render :new
+      end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @property.update(property_params)
+      redirect_to properties_path, notice: %(物件情報を更新しました！)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @property.destroy
+    redirect_to propertys_path, notice: %(物件情報を削除しました！)
+  end
+
+  private
+  def property_params
+    params.require(:property).permit(:name, :rent, :address, :age, :note)
+  end
+
+  def set_property
+    @property = Property.find(params[:id])
+  end
+end
